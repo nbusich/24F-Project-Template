fc0f8282d198c9786e79d479b534655fce5030de
nbusich <usich.n@northeastern.edu>
2024-12-01 19:59:31 -0500

    Finally got route 1 working

--- api/backend/admins/admin_routes.py
+++ api/backend/admins/admin_routes.py
@@ -1,13 +1,9 @@
-########################################################
-# Sample customers blueprint of endpoints
-# Remove this file if you are not using it in your project
-########################################################
 from flask import Blueprint
 from flask import request
 from flask import jsonify
 from flask import make_response
 from flask import current_app
-from api.backend.db_connection import db
+from backend.db_connection import db
 
 #------------------------------------------------------------
 # Create a new Blueprint object, which is a collection of 
--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -2,11 +2,8 @@ import logging
 logger = logging.getLogger(__name__)
 from modules.nav import SideBarLinks
 import streamlit as st
-import pandas as pd
-import altair as alt
 import requests
-import time
-from datetime import datetime
+
 
 
 st.set_page_config(layout = 'wide')
@@ -18,9 +15,7 @@ st.markdown("<div style='padding-top: -50px;font-size:18px; text-align:left; col
 
 data = {}
 
-response = requests.get('http://api:4000/dashboard')
-print(response.status_code)  # Print the HTTP status code
-print(response.text)         # Print the raw response text
+response = requests.get('http://api:4000/admin/changelog').json()
 
 st.dataframe(response)
 
