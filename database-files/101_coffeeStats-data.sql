913e4415e9b0a9b7d9146420e460ddf0f51f3933
nbusich <usich.n@northeastern.edu>
2024-12-01 20:53:28 -0500

    inserting fake data

--- database-files/101_coffeeStats-data.sql
+++ database-files/101_coffeeStats-data.sql
@@ -1,35 +1,117 @@
 use coffeeStats;
 
--- Populate the user table with at least four entries per role
-INSERT INTO user (role, email, userName, password) VALUES
-('student', 'student1@example.com', 'student1', 'pass1'),
-('student', 'student2@example.com', 'student2', 'pass2'),
-('student', 'student3@example.com', 'student3', 'pass3'),
-('student', 'student4@example.com', 'student4', 'pass4'),
-('administrator', 'admin@example.com', 'admin', 'adminpass'),
-('administrator', 'admin2@example.com', 'admin2', 'adminpass2'),
-('administrator', 'admin3@example.com', 'admin3', 'adminpass3'),
-('administrator', 'admin4@example.com', 'admin4', 'adminpass4'),
-('company', 'company@example.com', 'company1', 'compass'),
-('company', 'company2@example.com', 'company2', 'compass2'),
-('company', 'company3@example.com', 'company3', 'compass3'),
-('company', 'company4@example.com', 'company4', 'compass4'),
-('advisor', 'advisor@example.com', 'advisor1', 'advisorpass'),
-('advisor', 'advisor2@example.com', 'advisor2', 'advisorpass2'),
-('advisor', 'advisor3@example.com', 'advisor3', 'advisorpass3'),
-('advisor', 'advisor4@example.com', 'advisor4', 'advisorpass4'),
-('alumnus', 'alumnus@example.com', 'alumnus1', 'alumpass'),
-('alumnus', 'alumnus2@example.com', 'alumnus2', 'alumpass2'),
-('alumnus', 'alumnus3@example.com', 'alumnus3', 'alumpass3'),
-('alumnus', 'alumnus4@example.com', 'alumnus4', 'alumpass4'),
-('company', 'jbaker@example.com', 'bhlarma', 'compass5');
+INSERT INTO user (role, email, userName, password) VALUES ('Advisor', 'cruzgregory@yahoo.com', 'meganwhite', 'eLNLNyeM$69v');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'latashaperry@yahoo.com', 'halvarez', 'tu@6_1Ic*0dh');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'sandramartin@martinez-stafford.info', 'nbanks', 'EvD)6s!Dv*0V');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'brenda22@king.com', 'bridget05', '28BbtWDx&p*E');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'elizabeth54@martinez.com', 'edwardsjulia', 'z3@8YJLs*7rv');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'jasonarnold@gmail.com', 'john55', '9x4xIxf^)%fS');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'ogibson@guerra.com', 'bbarnes', '0sTKtFT%#Cuh');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'juan02@gmail.com', 'gschmitt', 'SHK0+CMu1Bg$');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'kfisher@hotmail.com', 'tranjohn', '%6+aCcSZm(Ds');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'ydavis@hotmail.com', 'kristopher60', '_7bNnwoNomF(');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'shaneserrano@davis-duke.com', 'jmullins', 'i8KQjaQE_XmG');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'pricejon@yahoo.com', 'holtdaniel', '@6iQPnLwuDy2');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'chrisnewman@turner.com', 'christinaking', '!z7jnuvi4pMP');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'sotoryan@gmail.com', 'ganderson', 'cv7R(ylE6&j^');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'erinfoster@bowman-ramos.com', 'amber01', 'F1s1vMmX!0#U');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'staffordlori@nguyen.net', 'nelsongabriela', '^9*P_tnl(iRz');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'gcole@hotmail.com', 'sfernandez', '36ETGisf%lHo');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'brent60@dean.com', 'eric67', 'y$UP%j#(@$06');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'kayla16@yahoo.com', 'sharon08', '_5UzwldNL5mQ');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'thomasanthony@montes.com', 'ericbridges', '#nmSXvqw5#6E');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'flopez@salas-fox.com', 'hthomas', 'QiJPL1Ci17d#');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'twhitney@hotmail.com', 'julie26', '6IN&7urboSe3');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'jason40@ramsey.com', 'ashley01', '$3E71sje9G8C');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'ismith@norris.com', 'dporter', 'yQcL+0Tx3*!B');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'edwardali@gonzalez.com', 'bradley32', 'I1$ERJAwj*Ib');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'ewilliamson@yahoo.com', 'matthewellis', 'hU!9g(Qx#0lC');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'qlarson@gmail.com', 'dianeshepherd', '^Gsji5cd_^7Q');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'uacosta@hotmail.com', 'masseytracy', '(4H#P5NlSdP8');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'kyoung@valenzuela.com', 'coreyoliver', 'HxGd4Ms%P4)h');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'brooke87@johnson.com', 'tburnett', '*4KsKj8s+wiv');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'martinezmichelle@gmail.com', 'xdrake', '!6gFlLQfs7&2');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'huffmanjoseph@hotmail.com', 'washingtonkimberly', '8Pf^l1^k@(Jq');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'johnsonjo@garrison.com', 'adamortega', '6CNGlQrd%R4@');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'owilliams@lester.com', 'qbrooks', '#kz&wvUm6xzu');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'gregoryhernandez@guzman.com', 'joneskim', 'F%#ibTtXth3k');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'kenneth57@hotmail.com', 'walljames', '3kicnVjw)cB0');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'sandra82@stokes-thompson.net', 'kenneth28', 'TBDr!*Mu*3o&');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'kelseycook@wright.info', 'hernandezmegan', 'oA@_2zDmy$e!');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'brianna40@parker.com', 'seanbell', '$XC!DeWqA7wV');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'richard46@yahoo.com', 'cwagner', '13RpAjX*(#gz');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'steven17@gmail.com', 'taylormitchell', 'lhHnb^!J!4%f');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'bakeranthony@gmail.com', 'mark06', '_C2Eyu!Bb_##');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'qpham@yahoo.com', 'tgreene', 'dBR4k0Z5*7dj');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'abrewer@gmail.com', 'jamie24', 'Yu*2$N#c+7(6');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'felicia22@yahoo.com', 'cwagner', 'ne49NekSYLA^');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'brennanedward@green.biz', 'cochransandra', 'K@5*LsdoZfRS');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'codygray@martinez-esparza.com', 'paula98', 'c!Z1ANZpMbgG');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'upierce@hotmail.com', 'shelby06', '$6tvMR%l@_2u');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'elizabeth63@carr.com', 'robert35', '@!S2Dx9vgF(A');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'william94@hotmail.com', 'turneranthony', 'N^&ZKiF6Tz4T');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'andersonkaitlyn@yahoo.com', 'kimberly14', 'RV6^zM6wZB#m');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'farrelllisa@anderson-vance.net', 'vwilliamson', 'n$Al33VzFoRt');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'brownjerry@gmail.com', 'novakmorgan', '+9V75#MgPo&5');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'ntaylor@yahoo.com', 'edwardcooley', '^8nP3HZc09X%');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'jsmith@hotmail.com', 'phammond', '(_YPRkTZ5$3Q');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'sarah95@hotmail.com', 'breannawashington', 'v%%c0#ykfF#!');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'austingilbert@hotmail.com', 'johnsonanthony', '*mAgOLXi6Z__');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'kimberlylee@hotmail.com', 'geoffreyrandolph', 'v3Zf)Xa%%7w2');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'daniel98@gmail.com', 'millerlauren', '!Y2aZ6lY0xi1');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'prestoncarly@gmail.com', 'nixonsamantha', 'aC%hQAb@Zc9+');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'williamwells@coleman.com', 'josephhoward', 'kyuZ^DKu&h1x');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'kevin93@hotmail.com', 'emily50', 'l1)8VIos)Zmp');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'lhamilton@sullivan.net', 'rwilliams', '#V&EoFlG6((M');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'jameslittle@hotmail.com', 'clinton30', '&a3IG864b9wG');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'stevencortez@yahoo.com', 'iandrade', '_4MBw+sNzHvx');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'melissashort@gmail.com', 'connor32', '&EOVFx&xP6HA');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'fernandezkendra@madden-delgado.com', 'barrydaniel', '(Ee8JRv3NTt%');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'zespinoza@hotmail.com', 'ptorres', '*P$lZm$U8$@T');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'millerzachary@gmail.com', 'erinmartinez', 'cA%AK8kt+6g6');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'erin80@anderson.biz', 'karen68', 'RWOL$wMu_3!r');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'baustin@gmail.com', 'abrown', 'q@W6bfz(%K6n');
+INSERT INTO user (role, email, userName, password) VALUES ('Company', 'jamie29@yahoo.com', 'yorksteven', 'xh(yPsv*@5db');
+INSERT INTO user (role, email, userName, password) VALUES ('Alumnus', 'waynesimon@thomas.com', 'manuel59', 'Y$#zQCgQbw#7');
+INSERT INTO user (role, email, userName, password) VALUES ('Advisor', 'jeffrey19@campbell.com', 'carlacook', 'Fn3#Shb&^(3_');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'moorekylie@gmail.com', 'michelle24', 'Cea6rFOll%7q');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'bobbyrose@wilson.com', 'longtracey', 'TF_zEBukRR74');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'christopher84@david.com', 'kellercurtis', '0ZK3K8VtgH+@');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'gutierrezricardo@hotmail.com', 'ashley05', 'D!1S1)%kuPNW');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'cynthia02@gmail.com', 'brandy67', 'N87Xhy$qEt^R');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'tgraves@smith.org', 'josephbrady', '_6w40L*m%dBy');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'david73@gmail.com', 'mcastillo', '^(BZGncBuM!5');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'christopher94@davis.com', 'gregory58', '&cgDAJ2w30GE');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'franklinaustin@sampson.com', 'douglasalvarado', 'L)i3j2YncpWE');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'cruznancy@gmail.com', 'jared25', '$8wcw)!P8ZK@');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'devinmcgee@hotmail.com', 'brian15', 'U8*T@G$j^#Q)');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'hthompson@garcia-craig.com', 'sharondunn', 'lFfNufAIW#9F');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'davidmckinney@moore.com', 'jerrydodson', 'O(pKhKbnycW4');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'sheilalewis@davidson.net', 'ritamitchell', '^2PGGzic66QB');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'marknixon@yahoo.com', 'sleonard', '^5ofQ%uk5Rci');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'woodsteve@johnson-chambers.net', 'tschultz', 'n#*O8ZdcXO0i');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'margaret40@cummings.com', 'jennamalone', '%_4GgW+RZ7E_');
+INSERT INTO user (role, email, userName, password) VALUES ('Administrator', 'april86@barnes.com', 'omiller', '+f1KE7k3g)D%');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'logan11@mann-edwards.org', 'dmartin', '*KI57NQhNA@i');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'foxdanny@choi.com', 'ryanking', '*(6hBb4ltXUu');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'willie40@yahoo.com', 'smoon', '_(7ZNlg%9(4v');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'anthony22@yahoo.com', 'jeff41', 'd2PCo!Vk@ELD');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'robert98@frank.org', 'klane', 'J)51h_9aC)$J');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'lisafrank@hotmail.com', 'fuentescynthia', '5B4Xvtfqu!5b');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'tanya92@lin.com', 'crystal08', '6_4iOd+lO%g6');
+INSERT INTO user (role, email, userName, password) VALUES ('Student', 'jeffrey21@mitchell.biz', 'zwilson', 'd&9&#$fZl^jJ');
 
--- Populate the administrator table
-INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES
-(5, 75000, 'Alice', 'Admin', '2022-01-01'),
-(6, 70000, 'Bob', 'Smith', '2022-02-01'),
-(7, 72000, 'Carol', 'Johnson', '2022-03-01'),
-(8, 68000, 'David', 'Williams', '2022-04-01');
+
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (35, 67644, 'Jeffrey', 'Daniel', '2024-05-27');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (69, 69123, 'Christopher', 'Walls', '2023-12-16');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (90, 70225, 'Timothy', 'Greene', '2023-10-15');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (104, 79042, 'Ashley', 'Burns', '2023-05-27');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (105, 66839, 'Christopher', 'Edwards', '2024-11-22');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (106, 66965, 'Jo', 'Chandler', '2023-01-29');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (107, 77252, 'Andrew', 'Burke', '2024-05-29');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (108, 65342, 'Angela', 'Hicks', '2024-07-27');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (109, 76720, 'Dwayne', 'Garcia', '2024-06-30');
+INSERT INTO administrator (id, salary, firstName, lastName, startDate) VALUES (110, 73772, 'Amanda', 'Greer', '2023-07-15');
 
 -- Populate the advisor table
 INSERT INTO advisor (id, firstName, lastName) VALUES
