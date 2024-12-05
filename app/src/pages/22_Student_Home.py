5c4a8b24f884af40b4601d1ef19878cb3e22f0d9
janinakurowski <kurowski.ja@northeastern.edu>
2024-12-05 02:08:16 -0500

    Update 22_Student_Home.py

--- app/src/pages/22_Student_Home.py
+++ app/src/pages/22_Student_Home.py
@@ -16,9 +16,9 @@ SideBarLinks()
 # Welcome
 st.title(f"Welcome, {st.session_state.get('first_name', 'Vinny')}!")
 
-# Link to pages
 st.write('### What would you like to do today?')
 
+# Link to pages
 if st.button('💼 Related Co-op Opportunities', type='primary', use_container_width=True):
     st.switch_page('pages/23_Coop_Opportunities.py')
 
@@ -28,29 +28,14 @@ if st.button('📅 Schedule an Advising Session', type='primary', use_container_
 if st.button('☕️ Connect with Alumni & Peers', type='primary', use_container_width=True):
     st.switch_page('pages/25_Connections.py')
 
-
- links = {
-        "Northeastern Co-op Portal": "https://northeastern-csm.symplicity.com/students/?signin_tab=0",
-        "Academic Calendar": "https://registrar.northeastern.edu/calendar",
-        "Student Support Services": "https://studentlife.northeastern.edu/",
-        "Career Development Resources": "https://careers.northeastern.edu/",
-        "University Libraries": "https://library.northeastern.edu/",
-        "Health and Counseling Services": "https://www.northeastern.edu/uhcs/",
-        "Student Clubs and Organizations": "https://northeastern.presence.io/",
-    }
-
-    for label, url in links.items():
-        st.markdown(f"- [{label}]({url})")
-
-    st.write("\n")
-
-if __name__ == "__main__":
-    display_student_links()
 st.write("\n")
 st.markdown(
     """
     <hr style="border:1px solid #eaeaea">
-    <small>"Need help? Contact the Co-op Office or access": "https://northeastern.edu/coop-faqs"</small>
+    <div style="text-align: center;">
+        <small>Need help? Contact the Co-op Office or visit our <a href="https://northeastern.edu/coop-faqs" target="_blank">FAQs</a>.</small>
+    </div>
     """,
     unsafe_allow_html=True,
 )
+        
